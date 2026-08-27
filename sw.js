b03fd9767732176a4d401decc2920c654c7589d4
Hermes <hermes@bot.local>
2026-08-27 09:07:05 +0000

    relative SW path + cache v16 (repo renamed)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Force-fresh SW: network-first for everything
-const CACHE = 'dakhel-v15';
+const CACHE = 'dakhel-v16';
 
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
