07e4b723a32d970c99e0ca47b4e6308e9fb4433f
Hermes <hermes@bot.local>
2026-08-27 07:59:43 +0000

    cache v13

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Force-fresh SW: network-first for everything
-const CACHE = 'dakhel-v12';
+const CACHE = 'dakhel-v13';
 
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
