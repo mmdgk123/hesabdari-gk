4f731ce46f12bb84b033483a7b74ba71d07f815e
Hermes <hermes@bot.local>
2026-08-28 18:12:45 +0000

    v20: replace month-compare with Top-5 spends insight card

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Force-fresh SW: network-first for everything
-const CACHE = 'dakhel-v19';
+const CACHE = 'dakhel-v20';
 
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
