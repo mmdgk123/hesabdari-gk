9761f5ac2cadec03c5dd7fa91c4c6597f8abb7e5
mmdgk123 <mmdgk123@users.noreply.github.com>
2026-09-03 18:20:32 +0000

    v23: بررسی آپدیت + لینک سازنده تلگرام

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Force-fresh SW: network-first for everything
-const CACHE = 'dakhel-v22';
+const CACHE = 'dakhel-v23';
 
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
