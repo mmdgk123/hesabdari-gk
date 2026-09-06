f9755f6e495f27acabbf6da4ea7effbe08eb05a7
mmdgk123 <mmdgk123@users.noreply.github.com>
2026-09-06 20:31:51 +0000

    v27: customizable accounts (add/rename/delete) + dakhel-v27

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Force-fresh SW: network-first for everything
-const CACHE = 'dakhel-v26';
+const CACHE = 'dakhel-v27';
 
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
