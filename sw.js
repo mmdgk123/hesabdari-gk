deaaa69150a682e5afcdf7fb1d8223ae9fb61d6c
mmdgk123 <mmdgk123@users.noreply.github.com>
2026-09-15 13:02:00 +0000

    v1.0 first stable release

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Force-fresh SW: network-first for everything
-const CACHE = 'dakhel-v27';
+const CACHE = 'dakhel-1.0';
 
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
