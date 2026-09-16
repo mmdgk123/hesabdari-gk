3c52517941d306e1d6f91cc19ae2d6ede48d3d7e
mmdgk123 <mmdgk123@users.noreply.github.com>
2026-09-16 16:03:27 +0000

    install guide button

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Force-fresh SW: network-first for everything
-const CACHE = 'dakhel-1.1';
+const CACHE = 'dakhel-1.2';
 
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
