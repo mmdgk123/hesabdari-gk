c9f82e864c00254c6e2d533bd913d476ad7d4974
Hermes <hermes@local>
2026-09-22 18:40:33 +0000

    chore: new gold GK app icon (real PNG), version 2.7

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Force-fresh SW: network-first for everything
-const CACHE = 'dakhel-2.6';
+const CACHE = 'dakhel-2.7';
 
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
