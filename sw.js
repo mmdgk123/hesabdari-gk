cd8d47cba61f5f3a9ec15f0852defc9dba6a398c
Hermes <hermes@local>
2026-09-22 18:56:05 +0000

    fix: force iOS to refetch home-screen icon (new touch-icon URL + cache-bust query), version 2.9

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Force-fresh SW: network-first for everything
-const CACHE = 'dakhel-2.8';
+const CACHE = 'dakhel-2.9';
 
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
